7f0bef3bc32c213aac38bcbf55ce700a628f64cd
Meenatchi-Mediwave <meenatchi@mindwaveventures.com>
2022-12-14 17:48:08 +0530

    add basic routing

--- app/index.js
+++ app/index.js
@@ -1,7 +1,49 @@
 const http = require("http");
 
+const books = [
+  {
+    id: 100,
+    name: "Harry Potter",
+  },
+];
+
+const animals = [
+  {
+    id: 300,
+    name: "Rabbit",
+  },
+];
+
 function requestHandler(req, res) {
-  res.write("Hello world");
+  /*
+  
+  /route
+  METHOD
+  query strings
+  route params
+  body payload
+  
+  */
+
+  const route = req.url;
+  const method = req.method;
+
+  console.log(method + " " + route);
+  res.setHeader("Content-Type", "application/json");
+
+  if (route === "/books") {
+    res.write(JSON.stringify(books));
+  } else if (route === "/animals") {
+    res.write(JSON.stringify(animals));
+  } else {
+    res.statusCode = 404;
+    res.write(
+      JSON.stringify({
+        message: "Route not found",
+      })
+    );
+  }
+
   res.end();
 }
 
